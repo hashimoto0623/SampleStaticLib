f4dda17c2a76077f79b66ae5bdfa7548903ac601
Shingo Hashimoto <hashimoto@brick-corp.com>
2016-11-16 12:52:26 +0900

    Initial Commit

---
+++ SampleStaticLib/SampleStaticLib.h
@@ -0,0 +1,13 @@
+//
+//  SampleStaticLib.h
+//  SampleStaticLib
+//
+//  Created by HashimotoShingo on 2016/11/16.
+//  Copyright © 2016年 Brick Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleStaticLib : NSObject
+
+@end
---
+++ SampleStaticLib/SampleStaticLib.m
@@ -0,0 +1,13 @@
+//
+//  SampleStaticLib.m
+//  SampleStaticLib
+//
+//  Created by HashimotoShingo on 2016/11/16.
+//  Copyright © 2016年 Brick Co., Ltd. All rights reserved.
+//
+
+#import "SampleStaticLib.h"
+
+@implementation SampleStaticLib
+
+@end
